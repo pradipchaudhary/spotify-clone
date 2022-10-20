91e5986daf38a67181b557fb4f1aae219dc62854
Pradip Chaudhary <chaudharypradip678@gmail.com>
2022-10-20 16:52:53 +0545

    spotify client id and api url include

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,12 +2,52 @@ import React from "react";
 import styled from "styled-components";
 
 export default function Login() {
+  const handleClick = () => {
+    const clientID = "49202d2100dd4e9688b6f0106f13f36e";
+    const redirectUrl = "http://localhost:3000/";
+    const apiUrl = "https://accounts.spotify.com/authorize";
+    const scope = [
+      "user-read-email",
+      "user-read-private",
+      "user-read-playback-state",
+      "user-read-currently-playing",
+      "user-read-recently-played",
+      "user-read-playback-position",
+      "user-top-read",
+    ];
+
+    alert(" Clicked...");
+  };
   return (
     <Container>
-      <img src="" alt="" />
-      <button>Connect Spotify </button>
+      <img
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"
+        alt=""
+      />
+      <button onClick={handleClick}>Connect Spotify </button>
     </Container>
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #1db954;
+  gap: 5rem;
+  img {
+    height: 20vh;
+  }
+  button {
+    padding: 1rem 5rem;
+    border-radius: 5rem;
+    border: none;
+    background-color: black;
+    color: #49f585;
+    font-size: 1.4rem;
+    cursor: pointer;
+  }
+`;
